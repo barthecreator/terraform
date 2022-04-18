185b9676b0453bafb9bbaeeb20f9ed2b5add7372
barthecreator <63727386+barthecreator@users.noreply.github.com>
2022-04-18 21:51:39 +0300

    Update basic.tf

--- basic.tf
+++ basic.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "bar-bucket-test-terra"
+    key    = "test/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
     region = "us-east-1"
 }
