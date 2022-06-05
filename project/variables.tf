98cb5795f7fdbb070f69080cec8da5e6c2548f09
barthecreator <63727386+barthecreator@users.noreply.github.com>
2022-06-05 15:58:31 +0300

    Create variables.tf

---
+++ project/variables.tf
@@ -0,0 +1,57 @@
+variable "vpc_a_cidr" {
+    type = string
+}
+
+variable "var_vpc_a_name" {
+    type = object({
+        Name = string
+    })
+}
+
+variable "var_subnet" {
+    type = string
+}
+
+variable "subnet_name" {
+    type = object({
+        Name = string
+    }) 
+}
+
+variable "igw_name" {
+    type = object({
+        Name = string
+    }) 
+}
+
+variable "ec2_name" {
+    type = object({
+        Name = string
+    })
+}
+
+variable "default-route" {
+    type = string
+  
+}
+
+variable "var_ami" {
+    type = string
+}
+
+variable "rt-name" {
+    type = object({
+        Name = string
+    }) 
+}
+
+variable "myip" {
+    type = string
+}
+
+variable "sg_name" {
+    type = object({
+        Name = string
+    })
+  
+}
